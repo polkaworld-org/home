d1d5d67866fbae79173caac50ff6ed37051e2c32
XatMassacrE <xxat.massacre@gmail.com>
2019-09-20 15:20:21 +0800

    Add registered name list area and default random icon

--- src/templates/pns.js
+++ src/templates/pns.js
@@ -3,6 +3,12 @@ import styled from 'styled-components';
 import searchIcon from '../images/search.svg';
 import { Layout } from '../components/common';
 import { MetaData } from '../components/common/meta';
+import Identicon from '@polkadot/react-identicon';
+import IconBox from '../components/IconBox';
+
+const baseUrl = 'https://laijiechan.com/api/v1/pns/'
+const imageBaseUrl = 'https://laijiechan.com/pub_media/'
+const kusamaUrl = 'https://polkascan.io/pre/kusama/account/'
 
 const Pns = ({ data, location }) => {
   const [name, setName] = useState('')
@@ -10,9 +16,17 @@ const Pns = ({ data, location }) => {
   const [available, setAvailable] = useState(false)
   const [address, setAddress] = useState('')
   const [mappedAddress, setMappedAddress] = useState('')
+  const [desc, setDesc] = useState('')
+  const [logo, setLogo] = useState(null)
+  const [registeredNames, setRegisteredNames] = useState([])
+  const [previewLogo, setPreviewLogo] = useState(null)
   const [errMsg, setErrMsg] = useState('')
   const [successMsg, setSuccessMsg] = useState('')
   const [submitErrMsg, setSubmitErrMsg] = useState('')
+
+  useEffect(() => {
+    fetchRegisteredNames();
+  }, []);
 
   function clearStatus() {
     setAvailable(false)
@@ -22,6 +36,32 @@ const Pns = ({ data, location }) => {
     setErrMsg('')
     setSubmitErrMsg('')
     setSuccessMsg('')
+    setPreviewLogo(null)
+    setDesc('')
+  }
+
+  const fileChangedHandler = event => {
+    setLogo(event.target.files[0])
+    let reader = new FileReader()
+    reader.onloadend = () => {
+      setPreviewLogo(reader.result)
+    }
+ 
+    reader.readAsDataURL(event.target.files[0])
+  }
+
+  async function fetchRegisteredNames() {
+    const url = `${baseUrl}query/all/`
+    fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        if (json.ok) {
+          setRegisteredNames(json.data)
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      }) 
   }
 
   function check() {
@@ -37,6 +77,11 @@ const Pns = ({ data, location }) => {
       setErrMsg('地址长度必须小于12')
       return false
     }
+    const pattern = /^[a-z0-9]*$/
+    if (!pattern.test(name)) {
+      setErrMsg('地址输入不合法')
+      return false
+    }
     setErrMsg('')
     return true
   }
@@ -47,7 +92,7 @@ const Pns = ({ data, location }) => {
       return
     }
 
-    const url = 'https://laijiechan.com/api/v1/pns/query/?name=' + name
+    const url = `${baseUrl}query/?name=${name}`
     fetch(url)
       .then(response => response.json())
       .then(json => {
@@ -75,14 +120,23 @@ const Pns = ({ data, location }) => {
     }
     setSubmitErrMsg('')
     setSuccessMsg('')
-    const url = `https://laijiechan.com/api/v1/pns/map/?name=${name}&address=${address}`
+    const url = `${baseUrl}map/`
 
-    fetch(url)
+    const fd = new FormData()
+    fd.append('logo', logo)
+    fd.append('name', name)
+    fd.append('desc', desc)
+    fd.append('address', address)
+    fetch(url, {
+      method: 'POST',
+      body: fd,
+    })
       .then(response => response.json())
       .then(json => {
         if (json.ok) {
           console.log('success')
           setSuccessMsg('注册成功')
+          fetchRegisteredNames()
         } else {
           setSubmitErrMsg(json.reason.err)
         }
@@ -103,7 +157,10 @@ const Pns = ({ data, location }) => {
               <input
                 type="text"
                 value={name}
-                onChange={e => setName(e.target.value)}
+                onChange={e => {
+                  clearStatus()
+                  setName(e.target.value)
+                }}
                 onKeyPress={event => {
                   if (event.key === 'Enter') {
                     query();
@@ -124,36 +181,60 @@ const Pns = ({ data, location }) => {
               queryName ? 
               <PnsCard>
                 <div className="name">
-                  <span>{name}.dot</span>
+                  <span>{name}.ksm</span>
                   <span className={available ? 'green' : 'red'}>
-                    {available ? '可用' : '不可用'}
+                    {available ? '地址可用，请赶快注册吧' : '地址已被注册'}
                   </span>
                 </div>
                 {
-                  available &&
-                  <div className="submit">
+                  !available ?
+                  <div className="address">
+                    <span>已映射地址：</span>
+                    <span>{mappedAddress}</span>
+                  </div>
+                  :
+                  <>
+                  <InputItem>
+                    <span>映射地址(必填): </span>
                     <input
                       type="text"
                       value={address}
-                      onChange={e => setAddress(e.target.value)}
+                      onChange={e => {
+                        setSubmitErrMsg('')
+                        setAddress(e.target.value)
+                      }}
                       onKeyPress={event => {
                         if (event.key === 'Enter') {
                           submit();
                         }
                       }}
-                      placeholder="请输入该域名映射的地址"
+                      placeholder="请输入该域名的映射地址"
                     />
-                    <button onClick={() => {
-                      submit();
-                    }}>免费注册</button>
-                  </div>
-                }
-                {
-                  !available &&
-                  <div className="address">
-                    <span>已映射地址：</span>
-                    <span>{mappedAddress}</span>
-                  </div>
+                  </InputItem>
+                  <InputItem>
+                    <span>域名简介: </span>
+                    <input
+                      type="text"
+                      value={desc}
+                      onChange={e => setDesc(e.target.value)}
+                      placeholder="请输入域名简介"
+                    />
+                  </InputItem>
+                  <LogoItem>
+                    <span>域名logo：</span>
+                    <input
+                      type="file"
+                      accept="image/jpeg, image/gif, image/png, image/bmp"
+                      onChange={fileChangedHandler}
+                    />
+                    {
+                      previewLogo && <img src={previewLogo} alt="icon" width="200" />
+                    }
+                  </LogoItem>
+                  <button onClick={() => {
+                    submit();
+                  }}>免费注册</button>
+                  </>
                 }
                 {
                   submitErrMsg && <ErrMsg><span>{submitErrMsg}</span></ErrMsg>
@@ -165,6 +246,43 @@ const Pns = ({ data, location }) => {
               :
               null
             }
+            <RegisteredArea>
+              {
+                registeredNames.map((item, index) => (
+                  <div className="registered-item" key={index}>
+                    {
+                      item.logo ?
+                      <img src={imageBaseUrl + item.logo} alt=''></img>
+                      :
+                      <IconBox
+                        icon={
+                          <Identicon
+                            className='icon'
+                            size={64}
+                            theme='substrate'
+                            value={item.address}
+                          />
+                        }
+                      />                   
+                    }
+                    <div className="detail">
+                      <div className="detail-item">
+                        <span className="detail-item-label">域名地址：</span>
+                        <span className="detail-item-value">{item.name}.ksm</span>
+                      </div>
+                      <div className="detail-item">
+                        <span className="detail-item-label">映射地址：</span>
+                        <a className="detail-item-value" href={kusamaUrl + item.address} target="_blank">{item.address}</a>
+                      </div>
+                      <div className="detail-item">
+                        <span className="detail-item-label">域名简介：</span>
+                        <span className="detail-item-value">{item.desc}</span>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              }
+            </RegisteredArea>
           </article>
         </div>
       </Layout>
@@ -181,7 +299,7 @@ const pnsStyle = {
 
 const SearchArea = styled.div`
   display: flex;
-  width: 60%;
+  width: 80%;
   position: relative;
   z-index: 10000;
 
@@ -199,9 +317,8 @@ const SearchArea = styled.div`
   input {
     padding: 10px 0 10px 55px;
     width: 80%;
-    /* border: none; */
     border-radius: 0;
-    font-size: 18px;
+    font-size: 20px;
     font-family: Overpass;
     font-weight: 100;
 
@@ -233,7 +350,7 @@ const SearchArea = styled.div`
 const ErrMsg = styled.div`
   display: flex;
   align-items: center;
-  width: 60%;
+  width: 80%;
   margin-top: 20px;
   span {
     color: red;
@@ -244,7 +361,7 @@ const ErrMsg = styled.div`
 const SuccessMsg = styled.div`
   display: flex;
   align-items: center;
-  width: 60%;
+  width: 80%;
   margin-top: 20px;
   span {
     color: green;
@@ -252,9 +369,47 @@ const SuccessMsg = styled.div`
   }
 `
 
+const LogoItem = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+  span {
+    width: 20%;
+  }
+  input {
+
+  }
+`
+
+const InputItem = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+  span {
+    width: 20%;
+  }
+  input {
+    padding: 10px 0 10px 10px;
+    width: 80%;
+    border-radius: 0;
+    font-size: 18px;
+    font-family: Overpass;
+    font-weight: 100;
+    &:focus {
+      outline: 0;
+    }
+    &::placeholder {
+      color: #ccd4da;
+    } 
+  }
+`
+
 const PnsCard = styled.div`
-  width: 60%;
-  height: 70px;
+  width: 80%;
   margin-top: 20px;
   display: flex;
   flex-direction: column;
@@ -274,23 +429,15 @@ const PnsCard = styled.div`
     margin-top: 20px;
   }
   .address {
+    width: 100%;
     margin-top: 20px;
-  }
-  input {
-    padding: 10px 0 10px 10px;
-    width: 80%;
-    border-radius: 0;
-    font-size: 18px;
-    font-family: Overpass;
-    font-weight: 100;
-    &:focus {
-      outline: 0;
-    }
-    &::placeholder {
-      color: #ccd4da;
-    } 
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
   }
   button {
+    margin-top: 20px;
+    margin-left: 40%;
     background: #1F215F;
     color: rgba(255, 255, 255, 0.7);
     font-size: 20px;
@@ -304,6 +451,53 @@ const PnsCard = styled.div`
       cursor: pointer;
       color: white;
     } 
+  }
+`
+
+const RegisteredArea = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 80px;
+  /* :nth-child(3n+1) {
+    margin-left: 0;
+  } */
+  .registered-item {
+    max-width: 350px;
+    width: 350px;
+    height: 100px;
+    padding: 14px;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid rgba(191, 191, 191, .7);
+    margin-top: 20px;
+    a, span {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap
+    }
+    img {
+      width: 50px;
+      height: 50px;
+    }
+    .detail {
+      display: flex;
+      flex-direction: column;
+      width: 250px;
+      .detail-item {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        .detail-item-label {
+          width: 75px;
+        }
+        .detail-item-value {
+          width: 165px;
+          text-align: right;
+        }
+      }
+    }
   }
 `
 
